207bd13b0d0ce7d240545c680f6d934b87d0497b
chocomilku <andrei.berja078@gmail.com>
2022-02-04 11:57:50 +0800

    added props to activities index page

--- pages/activities/index.tsx
+++ pages/activities/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
+import { directory, iProps, postItemProp } from "../../utils/exports";
+import fs from "fs";
+import matter from "gray-matter";
 
-const actHome = () => {
+const actHome = (props: postItemProp) => {
 	return (
 		<>
 			<Head>
@@ -13,3 +16,18 @@ const actHome = () => {
 };
 
 export default actHome;
+
+export const getStaticProps = async (): Promise<iProps> => {
+	const data: string[] = directory().map((page) => {
+		const path: string = `${process.cwd()}/posts/${page}`;
+		const content: string = fs.readFileSync(path, { encoding: "utf-8" });
+		return content;
+	});
+	const parse = data.map((item) => matter(item));
+	const metadata = parse.map((item) => item.data);
+	return {
+		props: {
+			metadata,
+		},
+	};
+};
--- utils/exports.ts
+++ utils/exports.ts
@@ -5,7 +5,24 @@ import fs from "fs";
  * @returns string array including the names of files with .md extension
  */
 export const directory = (): string[] => {
-	const dir: string[] = fs.readdirSync(`${process.cwd()}/activities`, "utf-8");
+	const dir: string[] = fs.readdirSync(`${process.cwd()}/posts`, "utf-8");
 	const files: string[] = dir.filter((file) => file.endsWith(".mdx"));
 	return files;
 };
+
+export interface postItemProp {
+	metadata: {
+		slug: string;
+		title: string;
+		description: string;
+		img: string;
+		date: string;
+		headers: string[];
+	};
+}
+
+export interface iProps {
+	props: {
+		[metadata: string]: any;
+	};
+}
