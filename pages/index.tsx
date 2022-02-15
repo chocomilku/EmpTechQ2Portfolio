f8d385c51e24000fb63917d2110de4bccf1847da
chocomilku <andrei.berja078@gmail.com>
2022-02-15 18:40:28 +0800

    added styles
    added cool hero at index, added font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
 	return (
@@ -8,7 +9,11 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Website Name</title>
 			</Head>
-			<h1 className="text-3xl font-bold">Home Page</h1>
+			<div className={styles.hero}>
+				<h1 className="">Home Page</h1>
+				<h2>Portfolio</h2>
+			</div>
+			<div>An Over-engineered portfolio using Next.js with Typescript</div>
 			<Link href="/activities">
 				<a>View All Activities</a>
 			</Link>
