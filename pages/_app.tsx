a8bda2035ab2428d0b40d18dfc62d8ad3dfb9575
chocomilku <andrei.berja078@gmail.com>
2022-02-16 21:47:07 +0800

    yoinked some navbar i found on the internet. will change it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Navbar } from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<ThemeProvider attribute="class">
+				<Navbar />
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</>
