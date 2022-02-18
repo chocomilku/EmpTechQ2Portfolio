fef42f7b3b10538bfd98dd78287b3cb99d440746
chocomilku <andrei.berja078@gmail.com>
2022-02-18 22:22:30 +0800

    clickable mobile hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,20 @@
 import { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.scss";
 
 export const Navbar: NextPage = ({ children }) => {
+	const [help, setHelp] = useState(false);
+	{
+		console.log(help);
+	}
 	return (
 		<div className="min-h-screen">
 			<div className="flex flex-col md:flex-row flex-1 min-h-screen w-full">
-				<div className="sticky top-0 md:hidden">
-					<div className="bg-purple-900 h-16 w-16 absolute z-10 top-0 right-0">
+				<div
+					className="fixed z-10 top-0 right-0 w-16 h-16 cursor-pointer bg-purple-900"
+					onClick={() => setHelp(!help)}>
+					<span className="relative">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							className="h-16 w-16"
@@ -20,7 +26,10 @@ export const Navbar: NextPage = ({ children }) => {
 								clipRule="evenodd"
 							/>
 						</svg>
-					</div>
+					</span>
+				</div>
+				<div className="md:hidden absolute w-screen h-screen bg-purple-900 text-white z-10">
+					<NavContent />
 				</div>
 				<main className="flex-1">{children}</main>
 				<LargerNavBar />
