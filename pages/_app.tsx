af705c3e0a734e611ce55c66b9b18909c034e6cc
chocomilku <andrei.berja078@gmail.com>
2022-02-18 19:07:42 +0800

    added page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,34 @@ import { ThemeProvider } from "next-themes";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Navbar } from "../components/Navbar";
+import { motion, AnimatePresence, Variants } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const variants: Variants = {
+	hidden: {
+		translateY: 100,
+		opacity: 0,
+	},
+	visible: {
+		translateY: 0,
+		opacity: 1,
+		transition: {
+			delay: 0.25,
+		},
+	},
+};
+
+function MyApp({ Component, pageProps, router }: AppProps) {
 	return (
 		<>
 			<ThemeProvider attribute="class">
 				<Navbar>
-					<Component {...pageProps} />
+					<motion.main
+						key={router.route}
+						variants={variants}
+						initial="hidden"
+						animate="visible">
+						<Component {...pageProps} />
+					</motion.main>
 				</Navbar>
 			</ThemeProvider>
 		</>
