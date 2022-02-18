e9eb272e8e29c1a7fe08fa85f1b3b3f647293585
chocomilku <andrei.berja078@gmail.com>
2022-02-18 23:03:04 +0800

    added some responsive design things

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.scss";
 
